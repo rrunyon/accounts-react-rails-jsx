a5729435239a07f1f1a258bfc475f7ff6628a5cb
Rick Runyon <rick.runyon@gmail.com>
2015-05-26 15:57:10 -0400

    refactor add/delete methods using react addons

--- app/assets/javascripts/components/records.js.jsx
+++ app/assets/javascripts/components/records.js.jsx
@@ -8,15 +8,14 @@ var Records = React.createClass({
   },
 
   addRecord: function(record) {
-    var records = this.state.records.slice();
-    records.push(record);
+    var records = React.addons.update(this.state.records, { $push: [record] })
     this.setState({ records: records });
   },
 
   deleteRecord: function(record) {
-    var records = this.state.records.slice();
-    var index = records.indexOf(record);
-    records.splice(index, 1);
+    var index = this.state.records.indexOf(record);
+    var records = React.addons.update(this.state.records,
+                                      { $splice: [[index, 1]] });
     this.replaceState({ records: records} );
   },
 
